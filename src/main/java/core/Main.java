daff0991de9f826c6abb26178763250b688605a8
mishabrsv <mshbrsv686@ya.ru>
2020-10-28 22:42:59 +0300

    TaskTwo class ended

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -3,6 +3,8 @@ package core;
 import tasks.TaskOne;
 import tasks.TaskTwo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -24,5 +26,14 @@ public class Main {
         System.out.println("2.2: " + TaskTwo.differenceMaxMin(mas));
         int[] mas1 = {1,5,6};
         System.out.println("2.3: " + TaskTwo.isAvgWhole(mas1));
+        int[] mas2 = {1,2,3,5};
+        System.out.println("2.4: " + TaskTwo.cumulativeSum(mas2));
+        System.out.println("2.5: " + TaskTwo.getDecimalPlaces("43.20"));
+        System.out.println("2.6: " + TaskTwo.fibonacci(7));
+        System.out.println("2.7: " + TaskTwo.isValid("59001"));
+        System.out.println("2.8: " + TaskTwo.isStrangePair("sparkling", "groups"));
+        System.out.println("2.9(prefix): " + TaskTwo.isPrefix("automation", "auto"));
+        System.out.println("2.9(suffix): " + TaskTwo.isSuffix("arochofobia", "fobia"));
+        System.out.println("2.10: " + TaskTwo.boxSeq(1));
     }
 }
--- src/main/java/tasks/TaskTwo.java
+++ src/main/java/tasks/TaskTwo.java
@@ -1,5 +1,8 @@
 package tasks;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class TaskTwo {
 
     public static String repeat(String text, int k) {
@@ -31,4 +34,118 @@ public class TaskTwo {
         if(sum%mas.length == 0) result = true;
         return result;
     }
+
+    public static ArrayList<Integer> cumulativeSum (int[] mas)
+    {
+        int sum = 0;
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int ma : mas) {
+            sum += ma;
+            arrayList.add(sum);
+        }
+        return arrayList;
+    }
+
+    public static int getDecimalPlaces(String str)
+    {
+        if (str.contains("."))
+        {
+            return str.length() - 1 - str.indexOf('.');
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static int fibonacci(int n)
+    {
+        switch (n)
+        {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            default:
+                int a = 1;
+                int b = 2;
+                int res;
+                for (int i = 3; i < n; i++)
+                {
+                    res = a + b;
+                    a = b;
+                    b = res;
+                }
+                return a + b;
+        }
+    }
+
+    public static boolean isValid(String str)
+    {
+        if (str.length() != 5)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (str.codePointAt(i) < '0' || str.codePointAt(i) > '9')
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isStrangePair(String str1, String str2)
+    {
+        if (str1.charAt(0) == str2.charAt(str2.length()-1) && str1.charAt(str1.length()-1) == str2.charAt(0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static boolean isSuffix(String word, String str)
+    {
+        str = str.substring(1);
+        if (word.endsWith(str))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static boolean isPrefix(String word, String str)
+    {
+        str = str.substring(0, str.length()-1);
+        if (word.startsWith(str))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static int boxSeq(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return n;
+        }
+        else
+        {
+            return n+2;
+        }
+    }
 }
+
